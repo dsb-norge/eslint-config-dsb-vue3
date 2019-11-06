489b0cdce38501097a491b661cd97c2fc401abe9
Nils Lien <nils.lien@dsb.no>
2019-11-06 15:07:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  extends: ['plugin:vue/recommended', 'standard'],
+  plugins: ['vue'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'semi': ['error', 'never'],
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': ['error', 'always-multiline'],
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': 'error',
+    'vue/v-on-function-call': 'error'
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
