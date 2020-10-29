dc13f6a224540ea639e0a5194719074fa52ddafb
Tommy Ballestad <tommy.ballestad@dsb.no>
2020-10-29 10:45:33 +0100

    Allow use of 'console.error' in logs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = {
     'vue'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error'] }] : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'never'],
