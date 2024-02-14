92e711b18dec6251a0258a57a14deb8fed9ee35d
Chris Aurora Neumann Ruud <66010392+chrisnruud@users.noreply.github.com>
2024-02-14 08:27:02 +0100

    feat: migrate rules to Stylistic

--- index.js
+++ index.js
@@ -6,19 +6,41 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'eslint:recommended',
+    'plugin:@stylistic/recommended-extends',
     'plugin:vue/vue3-recommended',
     'standard',
-    'plugin:vuejs-accessibility/recommended'
+    'plugin:vuejs-accessibility/recommended',
+    'plugin:@stylistic/disable-legacy'
   ],
   plugins: [
+    '@stylistic',
     'vuejs-accessibility'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error'] }] : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
+    '@stylistic/array-bracket-spacing': ['error', 'never'],
+    '@stylistic/arrow-parens': ['error', 'as-needed'],
+    '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    '@stylistic/comma-dangle': ['error', 'never'],
+    '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+    '@stylistic/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: false
+      },
+      singleline: {
+        delimiter: 'semi',
+        requireLast: false
+      },
+      multilineDetection: 'brackets'
+    }],
+    '@stylistic/object-curly-spacing': ['error', 'always'],
+    '@stylistic/padded-blocks': ['error', 'never'],
+    '@stylistic/quote-props': ['error', 'consistent-as-needed'],
+    '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+    '@stylistic/semi': ['error', 'never'],
+    '@stylistic/space-before-function-paren': ['error', 'always'],
     'vue/array-bracket-spacing': 'error',
     'vue/arrow-spacing': 'error',
     'vue/block-spacing': 'error',
